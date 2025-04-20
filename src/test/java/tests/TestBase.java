40744f4f2a0a008077c498c5ff264fa1401cd581
npronin <noxywille@yandex.ru>
2025-04-20 11:58:05 +0300

    fix

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -17,7 +17,8 @@ public class TestBase {
     @BeforeAll
     static void beforeAll(){
         Configuration.browser = System.getProperty("browser", "chrome");
-
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.browserVersion = System.getProperty("browserVersion", "127");
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.remote = "https://user1:1234@"+System.getProperty("selenoidUrl")+"/wd/hub/";
